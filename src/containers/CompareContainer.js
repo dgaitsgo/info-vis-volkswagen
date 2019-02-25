99b5a0944f26e67a67e9cf6f89c67e4b6402c363
Sebastian Kunz <skunz@e1z1r4p15.42.us.org>
2019-02-25 01:44:17 -0800

    Fullmodel scaffolding

--- src/containers/CompareContainer.js
+++ src/containers/CompareContainer.js
@@ -14,6 +14,7 @@ class CompareContainer extends Component {
         this.state = {
 			selectedModels: JSON.parse(this.props.location.pathname.split('/')[3])
 		}
+
 		console.log(this.state.selectedModels)
 	}
 
