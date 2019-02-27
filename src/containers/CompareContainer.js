fd2202747857b9d08e7120afb91332ad7e271528
Sebastian Kunz <skunz@e1z1r4p15.42.us.org>
2019-02-26 17:10:38 -0800

    compare Page

--- src/containers/CompareContainer.js
+++ src/containers/CompareContainer.js
@@ -4,9 +4,12 @@ import axios from 'axios'
 import Modal from 'react-modal'
 import { Loader } from 'react-bulma-components/full'
 import Description from '../components/Description'
+import data from './compareData.js'
 import Sidebar from '../components/Sidebar'
 import Option from '../components/Option'
 import ModelCard from '../components/ModelCard'
+import { Button } from "react-bulma-components/full"
+import _ from 'lodash'
 
 import Redirect from 'react-router-dom/Redirect'
 
@@ -18,23 +21,9 @@ Getting:
 			-id
 */
 
-const models = [
-	{
-		name: 'fucker1',
-		options: {
-			id: '1',
-			description: 'Nice ass'
-		}
-	},
-	{
-		id: '2',
-		description: 'Nice Butt'
-	},
-	{
-		id: '3',
-		description: 'Nice popo'
-	}
-]
+const sum = (accumulator, currentValue) => accumulator + currentValue
+
+const average = array => array.reduce(sum) / array.length
 
 class CompareContainer extends Component {
 
@@ -43,8 +32,8 @@ class CompareContainer extends Component {
 		super(props)
 
         this.state = {
-			fullModels : null,
-			openModals: {}
+			fullModels : data,
+			compareMode : 'CO2'
 		}
 	}
 
@@ -52,7 +41,7 @@ class CompareContainer extends Component {
 //		const urlData = this.props.location.pathname.split('/')
 //		const models = JSON.parse(urlData[3])
 //		const configurationIds = this.props.location.params.configurationIds
-//		
+//
 
 
 //		Promise.all(configurationIds.map(configId =>
@@ -62,7 +51,7 @@ class CompareContainer extends Component {
 //				}
 //		 })
 //		)).then(results => {
-//			 
+//
 //			let fullModels = []
 //
 //			results.forEach( (res, i) => {
@@ -101,18 +90,45 @@ class CompareContainer extends Component {
 		// const configurationIds = this.props.location.params.configurationIds
 
 		// configurationIds
-		axios.get('/api/configureModels', {
-			params : {
-				models
-			}
-		}).then(results => {
+		// axios.get('/api/configureModels', {
+		// 	params : {
+		// 		models
+		// 	}
+		// }).then(results => {
 
-			console.log(results.data)
-			this.setState({ configurations : results.data })
-		})
+		// 	console.log(results.data)
+		// 	this.setState({ configurations : results.data })
+		// })
 	}
-    }
 
+	setCompareMode = compareMode => this.setState({compareMode})
+
+	rankModels = () => {
+
+		const { fullModels, compareMode } = this.state
+		console.log('compare mode ', compareMode)
+		const averageModelMap = Object.keys(fullModels).map( (modelId, i) => {
+
+			const model = fullModels[modelId]
+			if (model.wltp.data.length) {
+
+				const currentInterps = model.wltp.data[0].interpolations.filter(interp => interp.value_type === compareMode)
+				console.log('current interps', currentInterps)
+				const interpAverage = average(currentInterps.map( item => item.value ))
+
+				return ({
+					modelId,
+					average: interpAverage
+				})
+			}
+
+			return null
+		}).filter(val => val)
+
+		_.sortBy(averageModelMap, 'average').reverse()
+
+		return (averageModelMap)
+	}
 
 	render() {
 
@@ -120,41 +136,41 @@ class CompareContainer extends Component {
 			fullModels
 		} = this.state
 
+		const rankedModels = this.rankModels()
+		const modelElems = rankedModels.map( (rankedModel, i) => {
+
+			const currModel = fullModels[rankedModel.modelId]
+
+		 	return (
+		 		<div className='compare-model-wrapper' key={i}>
+		 			<div className='compare-model-name-wrapper'>
+		 				<span className='compare-model-name'>
+		 					{`${i + 1}. ${currModel.model.name } `}
+		 				</span>
+						<span className='compare-model-value'>
+						{ rankedModel.average }
+						</span>
+		 			</div>
+		 		</div>
+		 	)
+		})
+
 		// if (!fullModels) {
 		// 	return (<Loader message="getting choices... "/> )
 		// }
 
 		return (
-			// <div className='compare-container-wrapper'>
-			// 	{ fullModels.map( (model, key) =>
-			// 		<div key={`compare-${key}`} className='compare-model-wrapper'>
-			// 			{ model.options.choices.map( (choice => {
-			// 				// choice.map(())
-			// 				return <div> NAME </div>
-			// 			}))}
-			// 			{/* { model.options.map( (name) => { */}
-			// 				{/* return <div> name </div> */}
-			// 			{/* }) } */}
-			// 		</div>
-			// 	)}
-			// </div>
-
 			<div className='compare-container-wrapper'>
-				<div className='build-wrapper'>
-					{models.forEach(model => {
-						console.log(model)
-						return (
-							<ModelCard
-								name={ model.name }
-								options={ model.options }
-							/>
-						)
-					})}
-				</div>
+			<div>
+				<Button onClick={() => this.setCompareMode('CO2')}>CO2</Button>
+				<Button onClick={() => this.setCompareMode('CONSUMPTION')}>Consumption</Button>
+			</div>
+				{modelElems}
 			</div>
 		)
 	}
 }
+
 
 export default CompareContainer
 
