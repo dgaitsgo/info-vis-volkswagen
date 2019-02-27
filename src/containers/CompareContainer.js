a2f9762328302e14bfb90b922fdc62e04b2365c6
David Gaitsgory <dgaitsgo@e1z3r3p5.42.us.org>
2019-02-26 21:53:39 -0800

    look for configs in local index

--- src/containers/CompareContainer.js
+++ src/containers/CompareContainer.js
@@ -5,6 +5,8 @@ import Description from '../components/Description'
 import Sidebar from '../components/Sidebar'
 import Option from '../components/Option'
 import Redirect from 'react-router-dom/Redirect'
+import getLocalStorage from '../modules/localStorage'
+import _ from 'lodash'
 
 class CompareContainer extends Component {
 
@@ -13,93 +15,127 @@ class CompareContainer extends Component {
 		super(props)
 
         this.state = {
-
 			fullModels : null
 		}
 	}
 
-	getChoices = (configId) => {
-//		const urlData = this.props.location.pathname.split('/')
-//		const models = JSON.parse(urlData[3])
-//		const configurationIds = this.props.location.params.configurationIds
-//		
+	//getChoices = (configId) => {
 
+	//	const urlData = this.props.location.pathname.split('/')
+	//	const models = JSON.parse(urlData[3])
+	//	const configurationIds = this.props.location.params.configurationIds
 
-//		Promise.all(configurationIds.map(configId =>
-//			axios.get('/api/choices', {
-//				params : {
-//					configurationId : configId
-//				}
-//		 })
-//		)).then(results => {
-//			 
-//			let fullModels = []
-//
-//			results.forEach( (res, i) => {
-//				fullModels.push({
-//					//get : model.id, model.name
-//					model : models[i],
-//					configId : configurationIds[i],
-//					options : res.data
-//				})
-//			})
-//
-//			console.log(fullModels)
-//
-//			this.setState({ fullModels })
-//
-//		 })
-//		 .catch(err => {
-//		 	const to = {
-//		 		pathname : '/server-error',
-//		 		query : {
-//		 			err
-//		 		}
-//		 	}
-//		 	return (
-//		 		<Redirect to={to} />
-//		 	)
-//		 })
-//    }
+	//	Promise.all(configurationIds.map(configId =>
+	//		axios.get('/api/choices', {
+	//			params : {
+	//				configurationId : configId
+	//			}
+	//	 })
+	//	)).then(results => {
+	//		 
+	//		let fullModels = []
 
+	//		results.forEach( (res, i) => {
+	//			fullModels.push({
+	//				//get : model.id, model.name
+	//				model : models[i],
+	//				configId : configurationIds[i],
+	//				options : res.data
+	//			})
+	//		})
+
+	//		console.log(fullModels)
+
+	//		this.setState({ fullModels })
+
+	//	 })
+	//	 .catch(err => {
+	//	 	const to = {
+	//	 		pathname : '/server-error',
+	//	 		query : {
+	//	 			err
+	//	 		}
+	//	 	}
+	//	 	return (
+	//	 		<Redirect to={to} />
+	//	 	)
+	//	 })
+	//}
+	
+	checkBuild = () => {
+		//to do 
 	}
 
-    componentDidMount() {
+	saveToLocal = () => {
+		
+		// todo
+		// fullModels.
+	}
+
+    async componentDidMount() {
 
 		const urlData = this.props.location.pathname.split('/')
 		const models = JSON.parse(urlData[3])
-		// const configurationIds = this.props.location.params.configurationIds
+		let modelsArr = Object.keys(models).map(modelId => ({ id : modelId, name : models[modelId] }))
+		console.log('looking for models', models)
 
-		// configurationIds
-		axios.get('/api/configureModels', {
-			params : {
-				models
-			}
-		}).then(results => {
+		//get local stage object
+		const ls = getLocalStorage('vw-product-data-configs')
 
-			console.log(results.data)
-			this.setState({ configurations : results.data })
+		ls.info().then(res => {
+
+			//look at the configurations the user has in loca
+			ls.find({ selector : { _id : 'configs' }})
+				.then(async res => {
+					
+					let fullModels = {}
+					let cachedModels = res.docs.length ? res.docs[0] : {}
+					
+					console.log('cached models', cachedModels)
+					//get the missing models
+					const missingModels = modelsArr.filter(model => !cachedModels[model.id])	
+
+					console.log('missing models', missingModels)
+					
+					if (missingModels.length) {
+						const fullModelsRes = await axios.get('/api/configureModels', {
+							params : {
+								models : missingModels
+							}
+						})
+
+						fullModels = fullModelsRes.data
+
+						console.log('missing model results', fullModels)
+					}
+						
+					fullModels = _.merge(cachedModels, fullModels)
+					fullModels._id = 'configs'
+
+					console.log('final fall models', fullModels)
+						
+					ls.insert(fullModels).then(_rev => {
+						fullModels._rev = _rev
+						this.setState({ fullModels })
+					})
+				})
 		})
 	}
 
 	render() {
 
 		const {
-			fullModels			
+			fullModels
 		} = this.state
 
 		if (!fullModels) {
 			return (<Loader message="getting options... "/> )
 		}
 
+		console.log(fullModels)
+
 		return (
-			<div className='compare-container-wrapper'>
-				{ fullModels.map( (model, key) =>
-					<div key={`compare-${key}`} className='compare-model-wrapper'>
-						{ JSON.stringify(model.options)}
-					</div>
-				)}				
-			</div>
+			null
 		)
 	}
 }
