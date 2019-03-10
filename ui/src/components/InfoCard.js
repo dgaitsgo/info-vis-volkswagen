a0a4f0868d2f805e5af029bad03865232710961e
Sebastian Kunz <skunz@e1z1r4p15.42.us.org>
2019-03-09 20:42:46 -0800

    add icons and more detailed reports for user

--- ui/src/components/InfoCard.js
+++ ui/src/components/InfoCard.js
@@ -3,12 +3,19 @@ import _ from 'lodash'
 import { Box, Button, Icon, Card, Heading, Media, Image } from 'react-bulma-components/full'
 
 import '../style/dashboard.css'
+import '../style/infocard.css'
 
 import ReactTooltip from 'react-tooltip'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { tire } from '@fortawesome/free-solid-svg-icons'
 
-const Interpolation = ({ data }) => {
+
+const ShowMoreInformation = ({ data, enabled}) => {
+	const interpolationClassName = enabled
+		? 'detailed-interpolation-wrapper open'
+		: 'detailed-interpolation-wrapper'
 	return (
-		<div>
+		<div className={ interpolationClassName }>
 			{data.map( entry => {
 				return (
 					<div>
@@ -43,18 +50,24 @@ class InfoCard extends Component {
 		super(props)
 
 		this.state = {
-			shouldShowMore: false,
+			showMoreEmissions: false,
+			showMoreConsumption: false
 		}
 	}
 
-	showMore = () => {
-		this.setState( { shouldShowMore: !this.state.shouldShowMore})
+	setMoreEmissions = () => {
+		this.setState( { showMoreEmissions: !this.state.showMoreEmissions})
+	}
+
+	setMoreConsumption = () => {
+		this.setState( { showMoreConsumption: !this.state.showMoreConsumption})
 	}
 
 	render(){
 
 		const {
-			shouldShowMore,
+			showMoreEmissions,
+			showMoreConsumption
 		} = this.state
 
 		const {
@@ -74,11 +87,12 @@ class InfoCard extends Component {
 
 		return (
 				<Card className='compare-model-wrapper'>
+
 					<Card.Header>
 						{ ranking === 0 && hasWltpData && <span className='icon ranking gold'><i className='fas fa-trophy'></i></span> }
 						{ ranking === 1 && hasWltpData && <span className='icon ranking silver'><i className='fas fa-trophy'></i></span> }
 						{ ranking === 2 && hasWltpData && <span className='icon ranking bronze'><i className='fas fa-trophy'></i></span> }
-						<Heading size={4}> {`${ranking + 1}. ${model.model.name } `} </Heading>
+						<Heading size={4}> { hasWltpData && `${ranking + 1}.`} { model.model.name } </Heading>
 						<Heading size={6}> {model.type.name} </Heading>
 						<Heading size={8}> {average} </Heading>
 					</Card.Header>
@@ -87,29 +101,49 @@ class InfoCard extends Component {
 							<Media.Item>
 							</Media.Item>
 						</Media>
+
 						<div>
 							{	hasWltpData
 									?
 									<div className='has-wltp-data-wrapper'>
 										<div>
-											{`Weight of Car ${generalData.value.toFixed(2)} ${generalData.unit}`}
+											<i class="fas fa-weight-hanging"></i> {generalData.value.toFixed(2)}{generalData.unit}
 										</div>
 										<div>
-											{`Fuel Type ${wltpData.fuel_types}`}
+											<i class="fas fa-gas-pump"></i> {wltpData.fuel_types}
 										</div>
 										<div className='compare-model-value'>
-											<span onClick={ this.showMore }> <Icon icon="angle-down" /> Detailed Interpolation </span>
+											<span onClick={ this.setMoreEmissions }> Detailed emissions<Icon icon="angle-down"/> </span>
+											{ showMoreEmissions
+												? <ShowMoreInformation
+													key={ranking}
+													data={ phases.map( phase => getInterpolations({ model: model, compareMode: 'CO2', phase}))}
+												/>
+												: null
+											}
+										</div>
+										<div>
+											<span onClick={ this.setMoreConsumption }><i class="fas fa-tint"/> Detailed consumption<Icon icon="angle-down"/></span>
+											{ showMoreConsumption
+												? <ShowMoreInformation
+													key={ranking}
+													data={ phases.map( phase => getInterpolations({ model: model, compareMode: 'CONSUMPTION', phase}))}
+												/>
+												: null
+											}
+										</div>
+										<div>
+											TireIcon class <Icon icon="angle-down"/>
+											{Object.keys(wltpData.tire).map( key => {
+												return (
+													<div>
+														{`${key}: ${wltpData.tire[key].category}`}
+													</div>
+												)
+											})}
 										</div>
 									</div>
 									: <NoData/>
-							}
-							{ shouldShowMore
-								? <Interpolation
-									key={ranking}
-									data={ phases.map( phase => getInterpolations({ model: model, compareMode, phase}))}
-									compareMode={ compareMode }
-								/>
-								: null
 							}
 						</div>
 						<Button
